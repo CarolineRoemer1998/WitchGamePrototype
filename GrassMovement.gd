e3aaaff9ac3457d153e70ed423e1d67c549cfd95
CarolineRoemer1998 <caroroemer@gmx.de>
2024-04-25 21:29:10 +0200

    Added Bushes and Grass
    Design inspired by TLoZ: Link's Awakening

---
+++ GrassMovement.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@onready var animation_player_wind: AnimationPlayer = $AnimationPlayerWind
+
+
+func _ready() -> void:
+	animation_player_wind.play("Wind")
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	animation_player_wind.play("Wind")
