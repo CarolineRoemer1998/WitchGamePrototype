c7f50446b7f220ca5481834cee2dbcc67865cafb
CarolineRoemer1998 <caroroemer@gmx.de>
2024-05-08 13:14:14 +0200

    Player drops onto Ground when spawning into Forest
    Movement is disabled for 0.75 seconds while dropping

--- Fundamentals/Game Behaviour/GameStates.gd
+++ Fundamentals/Game Behaviour/GameStates.gd
@@ -1,3 +1,4 @@
 extends Node3D
 
 var player_location = "Home"
+var player_movement_enabled = true
--- Player/player.gd
+++ Player/player.gd
@@ -8,6 +8,7 @@ class_name Player
 @onready var attack_component: Node3D = $AttackComponent
 @onready var block_component: Block = $Pivot/BlockComponent
 @onready var dmg_box: Area3D = $DmgBox
+@onready var timer_load_player: Timer = $TimerLoadPlayer
 
 var speed_walking = 7.0
 var speed_running = 12.0
@@ -23,6 +24,7 @@ var is_active = true
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+
 func _ready() -> void:
 	block_component.owner_type="Player"
 	# Set Layer to PlayerDmgBox
@@ -31,28 +33,34 @@ func _ready() -> void:
 	# Set Mask to EnemyAttack
 	dmg_box.set_collision_mask_value(7,true)
 	dmg_box.set_collision_mask_value(8,false)
-	
-	
-	
+
+
 func _physics_process(delta: float) -> void:
 	if is_active:
 		var input_dir := Input.get_vector("left", "right", "up", "down")
 		add_gravity(delta)
-		handle_jump()
-		handle_block()
-		handle_movement(input_dir)
-		rotate_and_move(input_dir)
-		handle_animations()
+		if GameStates.player_movement_enabled:
+			handle_jump()
+			handle_block()
+			handle_movement(input_dir)
+			rotate_and_move(input_dir)
+			handle_animations()
+		move_and_slide()
+
 
 func handle_animations():
-	if is_walking and is_on_floor():
-		animation_player.play("walk")
-	elif is_running and is_on_floor():
-		animation_player.play("run")
-	elif is_on_floor():
-		animation_player.play("idle")
+	if GameStates.player_movement_enabled:
+		if is_walking and is_on_floor():
+			animation_player.play("walk")
+		elif is_running and is_on_floor():
+			animation_player.play("run")
+		elif is_on_floor():
+			animation_player.play("idle")
+		else:
+			animation_player.play("RESET")
 	else:
 		animation_player.play("RESET")
+
 
 func add_gravity(delta:float):
 	if not is_on_floor():
@@ -62,6 +70,7 @@ func add_gravity(delta:float):
 func handle_jump():
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = sqrt(jump_height*2.0*gravity)
+
 
 func handle_block():
 	if Input.is_action_just_pressed("block"):
@@ -76,6 +85,7 @@ func handle_block():
 			if child is CollisionShape3D:
 				child.disabled = true
 		is_blocking = false
+
 
 func handle_movement(input_dir):
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
@@ -101,4 +111,14 @@ func handle_movement(input_dir):
 func rotate_and_move(input_dir):
 	if (is_walking or is_running):
 		pivot.look_at(position + Vector3(input_dir.x, 0, input_dir.y), Vector3.UP)
-	move_and_slide()
+
+
+func _on_timer_load_player_timeout() -> void:
+	if GameStates.player_location == "Forest":
+		GameStates.player_movement_enabled = false
+		velocity = Vector3.ZERO
+
+
+func _on_timer_enable_movement_timeout() -> void:
+	if GameStates.player_location == "Forest":
+		GameStates.player_movement_enabled = true
